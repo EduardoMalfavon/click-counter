a4d482c76e8f1f592493d9bed0a17a49094c04d8
Eduardo Malfavon Talavera <malfavontalaveraj@hotmail.com>
2021-06-12 18:23:30 -0500

    Started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("render without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(wrapper.exists()).toBe(true);
+  expect(appComponent.length).toBe(1);
 });
+
+test("render increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter display starts at 0", () => {});
+
+test("click button increments counter display", () => {});
