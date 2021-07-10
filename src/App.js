fe83f32d6f223e1cde34e08dbe27d770802d1e06
Eduardo Malfavon Talavera <malfavontalaveraj@hotmail.com>
2021-07-10 16:31:06 -0500

    ultimas configuraciones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [err, setErr] = useState("");
+
+  const onDecrement = () => {
+    if (count - 1 < 0) {
+      setErr(`Counter can't be negative numbers`);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  const onIncrement = () => {
+    if (err !== "") {
+      setErr("");
+      setCount(count + 1);
+    } else {
+      setCount(count + 1);
+    }
+  };
   return (
     <div data-test="component-app">
-      <h1>App</h1>
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
+      {err !== "" && <span>{err}</span>}
+      <button data-test="increment-button" onClick={() => onIncrement()}>
+        Increment counter
+      </button>
+      <button data-test="decrement-button" onClick={() => onDecrement()}>
+        Decrement counter
+      </button>
     </div>
   );
 }
